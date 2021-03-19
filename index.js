1c889782c039392027ec041534e2f1d1dd8a66d4
Zamomako <robin-delannoy_student2021@wilder.school>
2021-03-19 16:08:51 +0100

    changed index.html and information.html

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const chalk = require('chalk');
+const infoCampus = require('./information.js');
+console.log(infoCampus)
 
-console.log(chalk.blue('Hello, npm!'));
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : `Hi my name is ${infoCampus.nom}, I study at ${infoCampus.campus} campus`,
+	e : "oO",
+	T : "U "
+}));
+
--- information.js
+++ information.js
@@ -1,8 +1,8 @@
-let name = "Zamo";
-let age = 30;
-let hobby = ['skate', 'cooking', 'gaming'];
-const myInfos = () => {
-    console.log(`Hi my name is ${name}, I'm ${age} and my passions are ${hobby}`);
+const infoCampus = {
+    nom: "Rob",
+    campus: "Lille",
 };
 
-module.exports = myInfos;
+
+
+module.exports = infoCampus;
